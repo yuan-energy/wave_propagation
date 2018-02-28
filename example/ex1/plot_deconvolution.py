6c1f15b2bc132ab803cf67d3e7a47b163668275f
Yuan <fengyuan.energy@gmail.com>
2018-02-27 19:57:33 -0800

    updated figure format.

--- example/ex1/plot_deconvolution.py
+++ example/ex1/plot_deconvolution.py
@@ -2,6 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+# ===========================================
+# Font size
+# ===========================================
+import matplotlib
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 22}
+matplotlib.rc('font', **font)
+# ===========================================
+
 filename = sys.argv[1];
 print("Ploting file : " + filename)
 result = np.loadtxt(filename)
@@ -13,12 +23,14 @@ time = result[: ,0]
 ampl = result[:,1]
 surface_t = surface_wave[:,0]
 surface_a = surface_wave[:,1]
-plt.plot(time, ampl, linewidth=2, label="Deconvolution")
-plt.plot(surface_t, surface_a, linewidth=2, label="Surface")
+
+plt.figure(figsize=(16,12))
+plt.plot(time, ampl, 'r', linewidth=2, label="Deconvolution to Bedrock")
+plt.plot(surface_t, surface_a, 'k', linewidth=2, label="Surface")
 plt.legend()
 plt.xlabel("Time [second] ")
 plt.ylabel("Acceleration [m/s^2] ")
 plt.grid()
-plt.savefig("deconvolution.jpg")
+plt.savefig("deconvolution.pdf", transparent=True, bbox_inches='tight')
 plt.show()
 
